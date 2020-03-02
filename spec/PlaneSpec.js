eb534a905bee0061fd0ac80e00e3d6577b00a8d2
CodeKrakken <newholm1@hotmail.com>
2020-03-02 15:39:59 +0000

    Refactor plane tests

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -9,34 +9,38 @@ describe('Plane', function(){
     plane = new Plane();
     airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff','isAirport']);
     crab = jasmine.createSpyObj('crab',['walkSideways']);
-    plane._location = "in flight";
-    plane.land(airport);
-  });
-
-  it('can land at an airport', function(){
-    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
-  });
-
-  it('can be instructed to take off', function(){
-    plane.takeOff(airport);
-    expect(airport.clearForTakeOff).toHaveBeenCalledWith(plane);
+    plane._location = airport;
   });
 
   it('confirms departure', function(){
     expect(plane.takeOff(airport)).toEqual("Departure Successful.");
   });
 
-  it('will only land at an airport', function(){
-    plane.takeOff(airport);
-    expect(function(){plane.land(crab);}).toThrowError("Cannot land - invalid airport");
-  });
-
   it('will not land when already on ground', function(){
     expect(function(){plane.land(airport);}).toThrowError("Cannot land - already grounded");
   });
 
-  it('will not tke off when already in flight', function(){
-    plane.takeOff(airport);
-    expect(function(){plane.takeOff(airport);}).toThrowError("Cannot take off - already in flight");
+  describe('in flight', function(){
+
+    beforeEach(function(){
+      plane.takeOff(airport);
+    });
+
+    it('was successfully instructed to take off', function(){
+      expect(airport.clearForTakeOff).toHaveBeenCalledWith(plane);
+    });
+
+    it('can land at an airport', function(){
+      plane.land(airport);
+      expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+    });
+  
+    it('will only land at an airport', function(){
+      expect(function(){plane.land(crab);}).toThrowError("Cannot land - invalid airport");
+    });
+  
+    it('will not take off when already in flight', function(){
+      expect(function(){plane.takeOff(airport);}).toThrowError("Cannot take off - already in flight");
+    });
   });
 });
