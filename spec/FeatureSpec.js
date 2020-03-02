ceadc03eb081067da8967c62d2d99c13383ab499
CodeKrakken <newholm1@hotmail.com>
2020-03-02 20:26:57 +0000

    Refactor Feature tests

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -10,62 +10,65 @@ describe('Feature Test:', function(){
   beforeEach(function(){
     plane = new Plane();
     airport = new Airport();
-  });
+  })
 
-    it('planes can be instructed to land at an airport', function(){
-      spyOn(Math, 'random').and.returnValue(0);
+  describe('plane on ground', function(){
+    beforeEach(function(){
+      plane._location = airport;
+    })
+  
+    it('planes cannot take off if not at specified airport', function(){
+      airportTwo = new Airport();
+      expect(function(){plane.takeOff(airportTwo);}).toThrowError('Cannot take off - not at that airport');
+      expect(airportTwo.planes()).not.toContain(plane);
+    });
+  })
+  
+  describe('test must begin with plane in flight', function(){
+
+    beforeEach(function(){
       plane._location = 'in flight';
-      plane.land(airport);
-      expect(airport.planes()).toContain(plane);
     });
 
     it('planes will only land at an airport', function(){
-      plane._location = 'in flight';
       crab = jasmine.createSpyObj('crab', ['walkSideways']);
       expect(function(){plane.land(crab);}).toThrowError("Cannot land - invalid airport");
     });
 
-    it('planes can be instructed to take off', function(){
-      plane._location = airport;
-      spyOn(Math, 'random').and.returnValue(0);
-      plane.takeOff(airport);
+    it('airport blocks landing when weather is stormy', function(){
+      spyOn(Math, 'random').and.returnValue(1);
+      expect(function(){plane.land(airport);}).toThrowError('Cannot land during storm');
       expect(airport.planes()).not.toContain(plane);
     });
 
-    it('plane confirms departure', function(){
-      spyOn(Math, 'random').and.returnValue(0);
-      plane._location = airport;
-      expect(plane.takeOff(airport)).toEqual("Departure Successful.")
-    });
+    describe('in clement weather', function(){
+      beforeEach(function(){
+        spyOn(Math, 'random').and.returnValue(0);
+        plane.land(airport);
+      });
 
-    it('planes cannot land if airport is full', function(){
-      airport.alterCapacity(1);
-      spyOn(Math, 'random').and.returnValue(0);
-      planeTwo = new Plane();
-      planeTwo._location = "in flight";
-      plane._location = "in flight";
-      plane.land(airport);
-      expect(function(){planeTwo.land(airport);}).toThrowError('Cannot land - airport is full');
-      expect(airport.planes()).not.toContain(planeTwo);
-    });
-
-    it('planes cannot take off if not at specified airport', function(){
-      plane._location = airport;
-      expect(function(){plane.takeOff(airportTwo);}).toThrowError('Cannot take off - not at that airport');
-    });
-
-    it('airport blocks takeoff when weather is stormy', function(){
-      spyOn(Math, 'random').and.returnValue(0);
-      plane._location = "in flight";
-      plane.land(airport)
-      spyOn(airport._weather, 'isStormy').and.returnValue(true);
-      expect(function(){plane.takeOff(airport);}).toThrowError('Cannot take off during storm');
-      expect(airport.planes()).toContain(plane);
-    });
+      it('planes can be instructed to land at an airport', function(){
+        expect(airport.planes()).toContain(plane);
+      });
   
-    it('airport blocks landing when weather is stormy', function(){
-      spyOn(Math, 'random').and.returnValue(1);
-      plane._location = "in flight";
-      expect(function(){plane.land(airport);}).toThrowError('Cannot land during storm');
+      it('planes can be instructed to take off and confirm departure', function(){
+        expect(plane.takeOff(airport)).toEqual("Departure Successful.")
+        expect(airport.planes()).not.toContain(plane);
+      });
+  
+      it('airport blocks takeoff when weather is stormy', function(){
+        spyOn(airport._weather, 'isStormy').and.returnValue(true);
+        expect(function(){plane.takeOff(airport);}).toThrowError('Cannot take off during storm');
+        expect(airport.planes()).toContain(plane);
+      });
+  
+      it('planes cannot land if airport is full', function(){
+        airport.alterCapacity(1);
+        planeTwo = new Plane();
+        planeTwo._location = "in flight";
+        expect(function(){planeTwo.land(airport);}).toThrowError('Cannot land - airport is full');
+        expect(airport.planes()).not.toContain(planeTwo);
+      });
     });
+  });
 });
